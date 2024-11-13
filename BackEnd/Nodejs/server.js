baa58520e578e3708eb7a47bc5803a0206395799
Blen Bete <blenvpn09@gmail.com>
2024-11-13 13:58:46 +0300

    change server.js

--- BackEnd/Nodejs/server.js
+++ BackEnd/Nodejs/server.js
@@ -1566,7 +1566,7 @@ app.get("/api/submit-form", (req, res) => {
 app.use("/api/auth", authRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
